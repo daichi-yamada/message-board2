4a14aaa9dda9deea7a2a42302f746be3e88dffed
Daichi Yamada <yamada5123id@gmail.com>
2016-09-19 22:22:05 +0000

    finished

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,20 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only:[:edit, :update, :destroy]
   def index
     @message=Message.new
     @messages=Message.all
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      #保存に成功した場合はTOPへリダイレクト
+      redirect_to root_path ,notice:'編集完了'
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -15,8 +28,17 @@ class MessagesController < ApplicationController
     end
   end
   
+  def destroy
+    @message.destroy
+    redirect_to root_path, notice:"メッセージ削除しました"
+  end
+  
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :body, :age)
+  end
+  
+  def set_message
+    @message=Message.find(params[:id])
   end
 end
